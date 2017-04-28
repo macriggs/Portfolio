82a58bd5dfd7511a439845c015f05c216cb3b0b4
macriggs <macarthursignup@gmail.com>
2017-04-28 16:24:29 +1000

    Moved Nav and Header to php
    Moved Nav and Header elements to seperate PHP files

---
+++ head.php
@@ -0,0 +1,6 @@
+<head>
+        <meta charset="utf-8" />
+        <link rel="stylesheet" type="text/css" href="style/style.css">
+        <script src="js/animations.js"></script>
+        <title>Mac Riggs</title>
+    </head>
